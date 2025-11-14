291512b5f2b578939f539118634fa4f40a4f6b6b
Muhammad Ibtisam <ash.ibtisam@gmail.com>
2025-11-14 22:06:15 +0500

    Update gateway-stack-installation.sh

--- gateway-stack-installation.sh
+++ gateway-stack-installation.sh
@@ -26,6 +26,25 @@ echo "==============================================="
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.13.0/deploy/static/provider/cloud/deploy.yaml
 
 echo
+echo "===================================================="
+echo " Installing Traefik via Helm (NodePort 32080 / 32443)"
+echo "===================================================="
+
+# Install Helm plugins/repo
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
+chmod 700 get_helm.sh && ./get_helm.sh && rm -rf get_helm.sh
+helm repo add traefik https://traefik.github.io/charts
+helm repo update
+
+# Install Traefik
+helm install traefik traefik/traefik \
+  --namespace traefik \
+  --set ports.web.nodePort=32080 \
+  --set ports.websecure.nodePort=32443 \
+  --set service.type=NodePort \
+  --create-namespace
+
+echo
 echo "==============================================="
 echo " ✔ All components installed successfully! "
 echo "==============================================="
